f8d90a903aaa06a9f8f102a1a840b559836ad51b
Rodrigo P. Assunção <rodrigo.pereiraassuncao@gmail.com>
2015-04-02 16:36:24 -0300

    AboutViewController criado
    novamente só isso

---
+++ iPuzzle 0.2/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  iPuzzle 0.2
+//
+//  Created by Rodrigo P. Assunção on 4/2/15.
+//  Copyright (c) 2015 LEONARDO LEGRADY SALGADO GIGLIOTTI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
---
+++ iPuzzle 0.2/AboutViewController.m
@@ -0,0 +1,41 @@
+//
+//  AboutViewController.m
+//  iPuzzle 0.2
+//
+//  Created by Rodrigo P. Assunção on 4/2/15.
+//  Copyright (c) 2015 LEONARDO LEGRADY SALGADO GIGLIOTTI. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)btnBackClick:(id)sender {
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
