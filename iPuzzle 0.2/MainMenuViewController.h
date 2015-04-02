f1a9dd076448d897c71d44d8ceeed3113d00ce35
Rodrigo P. Assunção <rodrigo.pereiraassuncao@gmail.com>
2015-04-02 15:29:11 -0300

    Novo view controller
    é isso aí

---
+++ iPuzzle 0.2/MainMenuViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainMenuViewController.h
+//  iPuzzle 0.2
+//
+//  Created by Rodrigo P. Assunção on 4/2/15.
+//  Copyright (c) 2015 LEONARDO LEGRADY SALGADO GIGLIOTTI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *btnPlay;
+@property (weak, nonatomic) IBOutlet UIButton *btnAbout;
+
+@end
